74406b4576369d745f9500e8d6e3e6f7a7940574
Golden Zet <goldenzet@dhcpf239.fit.vutbr.cz>
2019-04-12 14:09:56 +0200

    Just basics

---
+++ main.py
@@ -0,0 +1,27 @@
+from moviepy.editor import *
+import moviepy.video.fx.all as vfx
+
+
+def intro_logo_with_background(background_animation, logo):
+    clip = VideoFileClip(background_animation)
+    clip = clip.resize(0.5)
+    #TODO resize for platform
+
+    sub = clip.subclip(0, 16)
+
+    clip2 = sub.speedx(final_duration=2)
+
+    clip3 = clip2.fx(vfx.time_mirror)
+
+    final = concatenate_videoclips([clip2, clip3])
+
+    image_clip = ImageClip(logo, duration=final.duration)
+    image_clip = image_clip.resize(0.3).set_position("center", "center")
+
+    final_video = CompositeVideoClip([final, image_clip.resize(lambda t: 1 + 0.1 * t)])
+
+    final_reverse = final_video.fx(vfx.time_mirror)
+    return final_reverse
+
+def product_previews(products, text ):
+    pass
