2b0f62a4982c08e1283e476d0ee594ae62de3559
Golden Zet <goldenzet@dhcpf239.fit.vutbr.cz>
2019-04-12 19:22:04 +0200

    Multiple files

--- flask_server.py
+++ flask_server.py
@@ -1,13 +1,21 @@
 import os
 from os.path import join, realpath, dirname
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'files/')
-ALLOWED_EXTENSIONS = ('txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4')
+HTML_FOLDER = join(dirname(realpath(__file__)), 'html/')
+BACKGROUND_FOLDER = join(dirname(realpath(__file__)), 'backgrounds/')
+LOGOS_FOLDER = join(dirname(realpath(__file__)), 'logos/')
+VIDEOS_FOLDER = join(dirname(realpath(__file__)), 'videos/')
+SONGS_FOLDER = join(dirname(realpath(__file__)), 'songs/')
+ALLOWED_EXTENSIONS = ('txt', "mp3", 'png', 'jpg', 'jpeg', 'gif', 'mp4')
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app = Flask(__name__, static_url_path='/html')
+app.config['HTML_FOLDER'] = HTML_FOLDER
+app.config['BACKGROUNDS'] = BACKGROUND_FOLDER
+app.config['LOGOS'] = LOGOS_FOLDER
+app.config['VIDEOS'] = VIDEOS_FOLDER
+app.config['SONGS'] = SONGS_FOLDER
 
 
 def allowed_file(filename):
@@ -15,35 +23,34 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-
-        if 'file' not in request.files:
+        if not request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+        for check_name in ("backgrounds", "logos", "songs", "videos"):
+            file_list = request.files.getlist(check_name)
+            if file_list:
+                for file in file_list:
+                    if file.filename == '':
+                        flash('No selected file')
+                        return redirect(request.url)
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename)
+                        file.save(os.path.join(app.config[check_name.upper()], filename))
+                return redirect(url_for("uploaded_file"))
+            else:
+                return send_from_directory(app.config['HTML_FOLDER'], 'index.html')
 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print("som tu")
-            return redirect(url_for("uploaded_file"))
+    elif request.method == 'GET':
+        return send_from_directory(HTML_FOLDER, 'index.html')
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
 
+@app.route('/download', methods=['GET', 'POST'])
+def download_file():
+    return send_from_directory(app.config['BACKGROUNDS'], "01.mp4")
 
 @app.route('/uploaded', methods=['GET'])
 def uploaded_file():
