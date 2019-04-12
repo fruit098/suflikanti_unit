c94be37097ee80fdad5f255474545282bc5e5042
xcalad01 <xcalad01@vutbr.cz>
2019-04-13 00:11:59 +0200

    Nic

--- video.py
+++ video.py
@@ -1,4 +1,4 @@
-import main
+import transition
 from moviepy.editor import *
 import moviepy.video.fx.all as vfx
 
@@ -7,5 +7,5 @@ list.append('/home/filipcaladi/Desktop/suflikanti_unit/images/newyork.jpg')
 list.append('/home/filipcaladi/Desktop/suflikanti_unit/images/stars.jpg')
 list.append('/home/filipcaladi/Desktop/suflikanti_unit/images/tma.jpg')
 
-video1 = main.one_on_each_other(list)#intro_logo_with_background('/home/filipcaladi/Desktop/suflikanti_unit/Dense_fog_ahead_of_Forest.mp4','/home/filipcaladi/Desktop/suflikanti_unit/logo6.png')
+video1 = transition.four_in_row('images/instagram_02.jpg')#intro_logo_with_background('/home/filipcaladi/Desktop/suflikanti_unit/Dense_fog_ahead_of_Forest.mp4','/home/filipcaladi/Desktop/suflikanti_unit/logo6.png')
 video1.write_videofile("test.mp4", fps=24)
