3503bab91a1f47f47ddc8f0df4519fc5abd5d03c
Miroslav Baláž <miroslav.balaz@live.com>
2019-04-12 11:54:40 +0200

    Added music_parser
    Generates list of dictionaries with mp3 name and duration time

--- music_parser.py
+++ music_parser.py
@@ -1,25 +1,26 @@
-from ID3 import *
+import eyed3
 from os import listdir
 from os.path import isfile, join
 
-import re
-
-
-album_music = list()
-mypath = "./input/"
-
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-
 ## Getting all files from folder
-for filename in onlyfiles:
-    album_music.append(filename)
+def create_album_record(path_to_mp3):
+    album_music = list()
+
+    final_output = list()
+    mypath = path_to_mp3
+
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    for filename in onlyfiles:
+        album_music.append(filename)
+
+    for mp3_file in album_music:
+        mp3 = eyed3.load(mypath + mp3_file)
+
+        duration = mp3.info.time_secs
+        final_output.append({"songname" : mp3_file, "duration" : duration})
+
+    return final_output
 
 
-try:
-    id3info = ID3(filename)
-    print id3info
-
-
-    
 
