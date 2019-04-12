cfa90aed5468dd2dcbbc1756f0e63dab52061da1
Golden Zet <goldenzet@dhcpf239.fit.vutbr.cz>
2019-04-12 16:42:28 +0200

    Basic flask files

--- flask_server.py
+++ flask_server.py
@@ -1,11 +1,60 @@
-import flask
+import os
+from os.path import join, realpath, dirname
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'files/')
+ALLOWED_EXTENSIONS = ('txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app.run()
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("som tu")
+            return redirect(url_for("uploaded_file"))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploaded', methods=['GET'])
+def uploaded_file():
+    return '''
+    <html>
+    <head></head>
+    <body>
+    <title style='display:block;'>File uploaded</title></body>
+    </html>
+    '''
+
+
+if __name__ == '__main__':
+    app.run()
