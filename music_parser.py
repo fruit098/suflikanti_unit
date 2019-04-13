b0cebfde61883fff26aafa21c27f594fc5b5ed22
Miroslav Baláž <miroslav.balaz@live.com>
2019-04-13 02:49:48 +0200

    music added

---
+++ music_parser.py
@@ -0,0 +1,65 @@
+from os.path import realpath
+import aubio
+import numpy as np
+from file_parser import create_album_record
+import sys
+
+import yodel.filter
+
+def avg_frequence(path_to_music):
+    downsample = 1
+    samplerate = 44100 // downsample
+
+    win_s = 4096 // downsample
+    hop_s = 512 // downsample
+
+    src = aubio.source(path_to_music, samplerate, hop_s)
+    samplerate = src.samplerate
+
+    pitch_o = aubio.pitch("yinfft", win_s, hop_s, samplerate)
+    
+    total_frames = 0
+    counter = 0
+    total_pitch = 0
+
+    while True:
+        counter += 1
+        samples, read = src()
+
+        pitch = pitch_o(samples)[0]
+
+        total_frames += read
+        total_pitch += pitch
+
+        if ((total_frames / float(samplerate)) > 5.1):
+            break
+
+    return (total_pitch/counter)
+
+def avg_bmp(music_folder):
+    music_folder = realpath(music_folder)
+
+    songs = create_album_record(music_folder)
+    min_density = sys.maxsize
+
+    for i in songs:
+        print(i)
+
+
+
+def set_intro_song(music_folder):
+    music_folder = realpath(music_folder)
+
+    songs = create_album_record(music_folder)
+    min_density = sys.maxsize
+
+    for i in songs:
+        density = avg_frequence(i["path"])
+        if density < min_density:
+            min_density = density
+            min_song = i
+
+    return min_song
+
+
+avg_bmp('./music/')
